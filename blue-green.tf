ed8ebb8ae2e4362af495a165f0babb4bf32961d2
Frans <msizafrans@gmail.com>
2023-09-29 02:43:37 +0000

    BlueGreenStrategy

--- blue-green.tf
+++ blue-green.tf
@@ -23,11 +23,3 @@ resource "aws_instance" "green" {
     Name = "Green Instance"
   }
 }
-
-output "blue_status" {
-  value = aws_instance.blue.state
-}
-
-output "green_status" {
-  value = aws_instance.green.state
-}
